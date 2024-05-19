0ace97d24375bf31dc405175478b6fe67ad6c8d6
= <=>
2024-05-19 14:16:06 +0530

    first commit

---
+++ src/app/Service/login.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { user } from '../Model/user.model';
+import { ActivatedRoute, Routes } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  userLoggedIn:boolean = false;
+  
+
+
+  users:user[]= [{email:'manik', password:"Manik@1998"}, 
+    {email:'saksham', password:'Saksham@2000'}
+  ]
+
+  constructor(private route: ActivatedRoute) { }
+
+  logincheck(email:string, password:string){
+    for (const user of this.users) {
+      if (user.email === email && user.password === password) {
+        this.userLoggedIn = true;
+      }
+    }
+
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
