53f4b1d203fb46857068caf1c19d13ebeaa38545
Manik Chib <manik.chib@orange.com>
2024-05-21 16:09:19 +0530

    optimised gui for add product page

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddproductComponent } from './addproduct/addproduct.component';
 import { ProductRoutingModule } from './product-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddproductComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    FormsModule
+    
   ]
 })
 export class ProductModule { }
